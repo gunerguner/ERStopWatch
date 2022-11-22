3508f98cf8e770c93f327f8d0ac4d8210698086b
ZhangZhicheng <delta_robin@hotmail.com>
2022-11-22 15:58:52 +0800

    add Package Swift

---
+++ Tests/ERStopWatchTests/ERStopWatchTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import ERStopWatch
+
+final class ERStopWatchTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+//        XCTAssertEqual(ERStopWatch, "Hello, World!")
+    }
+}
